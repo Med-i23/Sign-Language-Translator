dce9613418fed9abb90ebb44fbc095217eb8336a
Csete Medárd <csetemedard@gmail.com>
2024-09-29 10:11:50 +0200

    #10 Current fine tuning checkpoint

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 import pandas as pd
-
+import numpy as np
 from keras.src.models import Sequential
 from keras.src.layers import Dense, Conv2D, Flatten , Dropout , BatchNormalization
 from keras.src.layers.pooling.max_pooling2d import MaxPooling2D
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from keras.src.callbacks import ReduceLROnPlateau
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import classification_report
 
 train_df = pd.read_csv("dataset/sign_mnist_train.csv")
 test_df = pd.read_csv("dataset/sign_mnist_test.csv")
@@ -38,17 +40,16 @@ datagen = ImageDataGenerator(
         featurewise_std_normalization=False,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
         zca_whitening=False,  # apply ZCA whitening
-        rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.1, # Randomly zoom image
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1, # randomly shift images vertically (fraction of total height)
-        brightness_range=[0.8, 1.2],  # brightness adjusting
-        horizontal_flip=False,  # randomly flip images
+        rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.2, # Randomly zoom image
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
         vertical_flip=False)  # randomly flip images
 
 datagen.fit(x_train)
 
-learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience = 2, verbose=1,factor=0.5, min_lr=0.00001)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience = 1, verbose=1,factor=0.5, min_lr=0.00001)
 
 # Convolution
 model = Sequential()
@@ -73,7 +74,6 @@ model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D((2, 2), padding='same'))
 
-# Flattening from 3D to 1D
 model.add(Flatten())
 
 # First dense layer
@@ -85,16 +85,23 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.4))
 
 model.add(Dense(units = 24 , activation = 'softmax'))
-
 model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
-
 model.summary()
 
+# Calculateing class weights
+class_weights = compute_class_weight('balanced', classes=np.unique(y_train.argmax(axis=1)), y=y_train.argmax(axis=1))
+class_weights = {i: class_weights[i] for i in range(len(class_weights))}
+
 # Train to model
-history = model.fit(datagen.flow(x_train,y_train, batch_size = 128) ,epochs = 20 , validation_data = (x_test, y_test) , callbacks = [learning_rate_reduction])
+history = model.fit(datagen.flow(x_train,y_train, batch_size = 128), epochs = 20, validation_data = (x_test, y_test), class_weight=class_weights, callbacks = [learning_rate_reduction])
 
 # Save the trained model
 model.save('models/smnist.keras')
 
 print("Accuracy of the model is - " , model.evaluate(x_test,y_test)[1]*100 , "%")
 
+y_pred = model.predict(x_test)
+y_pred_classes = y_pred.argmax(axis=1)
+y_true = y_test.argmax(axis=1)
+
+print(classification_report(y_true, y_pred_classes))
